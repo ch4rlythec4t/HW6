16ff080c360b1a61009b3d02be5599daf460692c
Sergey Vorobev <ch4rlythec4t@gmail.com>
2022-08-24 21:40:47 +0300

    Task 41 done

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    static int[] CreateArray()
+    {
+        Console.WriteLine("Введите числа через запятую: ");
+        string? InputNums = Console.ReadLine();
+
+        string[] StringNums = InputNums!.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+        int[] IntNums = new int[StringNums.Length];
+
+        for (int i = 0; i < StringNums.Length; i++)
+        {
+            IntNums[i] = int.Parse(StringNums[i]);
+        }
+        return IntNums;
+    }
+
+    static int Counter(int[] CountArray)
+    {
+        int count = 0;
+        for (int i = 0; i < CountArray.Length; i++)
+        {
+            if (CountArray[i] > 0) count++;
+        }
+        return count;
+    }
+
+    static void Main()
+    {
+        int[] NewArray = CreateArray();
+        Console.Write("[" + String.Join(", ", NewArray) + "] -> " + Counter(NewArray));
+    }
+}
